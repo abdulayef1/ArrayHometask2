68f4eaccc3340718f55f9a8bfce5e75f95145b5b
abdulayef1 <7ka1cy@.code.edu.az>
2022-11-15 18:39:19 +0400

    array home task2 created

---
+++ HomeTask2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("input length:");
+int length = int.Parse(Console.ReadLine());
+int[] numbers= new int[length];
+int sumOfEvens = 0;
+
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine("intput "+(i+1)+". number:");
+    numbers[i]=int.Parse(Console.ReadLine());
+
+
+    if (numbers[i]%2==0)
+    {
+        sumOfEvens += numbers[i];
+    }
+}
+
+Console.WriteLine("Sum of even numbers:"+sumOfEvens);
+
